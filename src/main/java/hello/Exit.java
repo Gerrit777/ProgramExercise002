d4a462801c61863df76827f421642db6c28eba99
Gerrit Brands <g.brands1@chello.nl>
2017-12-14 18:41:34 +0100

    /shutdown implemented

--- src/main/java/hello/Exit.java
+++ src/main/java/hello/Exit.java
@@ -1,14 +1,11 @@
 package hello;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.ExitCodeGenerator;
-
 public class Exit {
 
     private final long id;
     private final String content;
 
-    public Exit(long id, String content) {
+    Exit(long id, String content) {
         this.id = id;
         this.content = content;
         //This appears in console !! When http://localhost:8080/GerritTest2 is entered in the browser
@@ -21,7 +18,5 @@ public class Exit {
     public String getContent() {
         return content;
     }
-
-
 }
 
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -2,9 +2,7 @@ package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.boot.ExitCodeGenerator;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ApplicationContext;
+
 import org.springframework.web.bind.annotation.*;
 
 
@@ -15,25 +13,21 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/GerritTest1")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+                String.format(template, name));
     }
+
     @RequestMapping("/GerritTest2")
-    public Exit exit(@RequestParam(value="name", defaultValue="Amerika") String name) {
+    public Exit exit(@RequestParam(value = "name", defaultValue = "Amerika") String name) {
         return new Exit(counter.incrementAndGet(),
                 String.format(template, name));
     }
 
-
     @RequestMapping("/shutdown")
-    public static ApplicationContext context;
-        {
-            ExitCodeGenerator args;
-            new ExitCodeGenerator(exit(args);
-            SpringApplication.exit(Application.class,exitCodeGenerator);
-        System.out.println("done");
-        }
+    public void shutdown(){
+        System.exit(500);
     }
+}
 
 
